0eb2e97f1d9607a27f687ec8073b41c09d43696d
Rana AlQatari <ralqatari@suffolk.edu>
2018-03-14 13:27:08 -0400

    added comment to feature page

--- feature.php
+++ feature.php
@@ -1,5 +1,5 @@
 <?php
-/* This is a multi line comment in PHP with yet another line of comment */
+/* This is a multi line comment in PHP with yet another line of comment  */
 
 echo "<h1>This is our feauture page!</h1>"
 phpinfo();
